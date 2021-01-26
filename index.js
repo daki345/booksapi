8d629158376942920bda615523c2a45353f6fcc7
lenovo <ddaki331@rku.ac.in>
2021-01-26 15:06:54 +0530

    added port

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ const route = require('./route');
 
 
 //connection to mongodb
-mongoose.connect("mongodb://localhost:27017/emp",{useNewUrlParser:true,useUnifiedTopology:true}).then(
+mongoose.connect("mongodb+srv://daki:Daki#123@cluster0.il17x.mongodb.net/Student?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true}).then(
     () => {
         const app = express();
         app.use("/api",route);
-        app.listen(3000,() =>{
+        app.listen(process.env.PORT || 3000,() =>{
             console.log("server started..!!");
         })
     }
