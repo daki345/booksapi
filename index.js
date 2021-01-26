f8e12072e76937af5057f3e508b22ae84b04888e
lenovo <ddaki331@rku.ac.in>
2021-01-26 15:18:45 +0530

    again

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const route = require('./route');
 
 
 //connection to mongodb
-mongoose.connect("mongodb+srv://daki:Daki#123@cluster0.il17x.mongodb.net/Student?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true}).then(
+mongoose.connect("mongodb+srv://daki:Daki@12345@cluster0.il17x.mongodb.net/Student?retryWrites=true&w=majority",{useNewUrlParser:true,useUnifiedTopology:true}).then(
     () => {
         const app = express();
         app.use("/api",route);
@@ -13,3 +13,14 @@ mongoose.connect("mongodb+srv://daki:Daki#123@cluster0.il17x.mongodb.net/Student
         })
     }
 );
+
+//connection to mongodb
+// mongoose.connect("mongodb://localhost:27017/emp",{useNewUrlParser:true,useUnifiedTopology:true}).then(
+//     () => {
+//         const app = express();
+//         app.use("/api",route);
+//         app.listen(process.env.PORT || 3000,() =>{
+//             console.log("server started..!!");
+//         })
+//     }
+// );
